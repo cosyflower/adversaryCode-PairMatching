7f53cca33a5ffee4f051dc1ae23c82de538731b5
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 23:11:44 +0900

    feat(Crew): 크루들의 이름, 과정을 관리하는 클래스 생성

---
+++ src/main/java/pairmatching/domain/Crew.java
@@ -0,0 +1,11 @@
+package pairmatching.domain;
+
+public class Crew {
+    private final String crewName;
+    private final Course course;
+
+    public Crew(String crewName, Course course) {
+        this.crewName = crewName;
+        this.course = course;
+    }
+}
