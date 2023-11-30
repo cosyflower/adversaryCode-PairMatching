a7a36c623a4ec052305cc8ce383da06b34de7adf
sunghun <nokdonwithkj211@gmail.com>
2023-11-30 22:09:40 +0900

    feat(Course): 코스 정보를 저장하는 Course 생성

--- src/main/java/pairmatching/Course.java
+++ src/main/java/pairmatching/Course.java
@@ -1,2 +1,13 @@
-package pairmatching;public class Course {
+package pairmatching;
+
+public enum Course { // 17M
+    BACKEND("백엔드"),
+    FRONTEND("프론트엔드")
+    ;
+
+    private final String courseName;
+
+    Course(String courseName) {
+        this.courseName = courseName;
+    }
 }
