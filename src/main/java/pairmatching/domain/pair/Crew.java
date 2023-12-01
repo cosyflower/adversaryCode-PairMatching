624c66de428d281f189e21ad810238132d73ed38
sunghun <nokdonwithkj211@gmail.com>
2023-12-01 09:00:13 +0900

    feat(Crew): 이름, 과정이 같다면 같은 Crew로 수정

--- src/main/java/pairmatching/domain/pair/Crew.java
+++ src/main/java/pairmatching/domain/pair/Crew.java
@@ -1,5 +1,6 @@
 package pairmatching.domain.pair;
 
+import java.util.Objects;
 import pairmatching.domain.Course;
 
 public class Crew {
@@ -9,5 +10,22 @@ public class Crew {
     public Crew(String crewName, Course course) {
         this.crewName = crewName;
         this.course = course;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Crew crew = (Crew) o;
+        return Objects.equals(crewName, crew.crewName) && course == crew.course;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(crewName, course);
     }
 }
