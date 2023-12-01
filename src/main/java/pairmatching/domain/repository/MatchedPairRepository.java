d2d032493078adfb897f7de8fb2cca9e7aedb47d
sunghun <nokdonwithkj211@gmail.com>
2023-12-01 09:38:31 +0900

    feat(MatchedPairRepository): 과정, 미션에 따라 저장된 Pair가 존재하는지, 존재하는 Pair 찾는 메서드 추가

--- src/main/java/pairmatching/domain/repository/MatchedPairRepository.java
+++ src/main/java/pairmatching/domain/repository/MatchedPairRepository.java
@@ -2,9 +2,11 @@ package pairmatching.domain.repository;
 
 import java.util.ArrayList;
 import java.util.List;
+import pairmatching.domain.Course;
+import pairmatching.domain.Mission;
 import pairmatching.domain.pair.MatchedPair;
 
-public class MatchedPairRepository {
+public class MatchedPairRepository { // 01 52
     public final List<MatchedPair> matchedPairs; // Level, List<Pair> 정보를 가지고 있다
 
     public MatchedPairRepository() {
@@ -19,5 +21,20 @@ public class MatchedPairRepository {
 
     public void addNewPair(MatchedPair uncheckedMatchedPair) {
         matchedPairs.add(uncheckedMatchedPair);
+    }
+
+    public MatchedPair findMatchedPair(Course course, Mission mission) {
+        return matchedPairs.stream()
+                .filter(matchedPair -> matchedPair.hasSameCourse(course))
+                .filter(matchedPair -> matchedPair.hasSameMission(mission))
+                .findFirst()
+                .get();
+    }
+
+    public boolean hasMatchedPair(Course course, Mission mission) {
+        return matchedPairs.stream()
+                .filter(matchedPair -> matchedPair.hasSameCourse(course))
+                .filter(matchedPair -> matchedPair.hasSameMission(mission))
+                .count() > 0;
     }
 }
